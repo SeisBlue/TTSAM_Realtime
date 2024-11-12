12f54cc8ee79af12bc4b1b758d430f898127fd6a
jimmy <jimmy60504@gmail.com>
2024-11-13 00:40:16 +0800

    remove port

---
+++ test_ttsam.sh
@@ -0,0 +1,9 @@
+docker run \
+-v $(pwd):/workspace \
+-v /opt/Earthworm/run/params:/opt/Earthworm/run/params:ro \
+--rm \
+--ipc host \
+--net=host \
+--name ttsam-cpu \
+seisblue/ttsam-realtime \
+/opt/conda/bin/python3 -m unittest discover /workspace/tests
